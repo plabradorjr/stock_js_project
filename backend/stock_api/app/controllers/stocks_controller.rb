b9bf67ca1da266aad229c39b7473649b0a64fa13
Pancho Labrador <plabradorjr@gmail.com>
2020-08-19 20:48:01 -0500

    fixing routes

--- backend/stock_api/app/controllers/stocks_controller.rb
+++ backend/stock_api/app/controllers/stocks_controller.rb
@@ -2,7 +2,32 @@ class StocksController < ApplicationController
 
 
     def index
+        # Stock.scrapeAndSaveYahooFinance
+        @stocks = Stock.all
+        render :index
+    end
+
+    def show
         
+    end
+
+    private
+
+     def scrape_and_save_yahoo_finance
+
+            doc = Nokogiri::HTML(open("https://finance.yahoo.com/gainers"))
+
+            table = doc.css("tbody")
+            rows = table.css("tr.simpTblRow")
+
+            rows.each do |stock|
+                stock_ticker =  stock.css("a")[0].text
+                stock_change = stock.css("span")[2].text
+                stock_name = stock.css("td")[1].text
+                stock_price = stock.css("span")[0].text
+
+                Stock.create(name: stock_name, ticker: stock_ticker, price: stock_price, change: stock_change)
+            end
     end
 
 end
